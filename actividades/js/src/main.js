6911a86aa4b15366e3bd7e83a5fa6a76be2065ea
Alanreb <alanrebgradas@gmail.com>
2024-09-26 14:28:40 -0600

    Carpetas creadas y primeros ejercicios realizados

---
+++ actividades/js/src/main.js
@@ -0,0 +1,59 @@
+function getDatos(){
+    var nombre = window.prompt("Nombre: ", "");
+    var edad = window.prompt("Edad: ", "");
+
+    var div1 = document.getElementById('nombre');
+    div1.innerHTML = '<h3> Nombre: '+ nombre + '</h3>';
+
+    var div2 = document.getElementById('edad');
+    div2.innerHTML = '<h3> Edad: '+ edad + '</h3>';
+}
+
+function HolaMundo(){
+    document.write('Hola Mundo');
+}
+
+function Juan(){
+    var nombre = 'Juan';
+    var edad = 10;
+    var altura = 1.92;
+    var casado = false;
+    document.write(nombre);
+    document.write( '<br>' );
+    document.write(edad);
+    document.write('<br>');
+    document.write(altura);
+    document.write('<br>');
+    document.write(casado);
+}
+
+function EntradadeDatos(){
+    var nombre;
+    var edad;
+    nombre = prompt('Ingresa tu nombre:', '');
+    edad = prompt('Ingresa tu edad:', '');
+    document.write('Hola ');
+    document.write(nombre);
+    document.write(' así que tienes ');
+    document.write(edad);
+    document.write(' años');
+}
+
+function SumaProducto(){
+    var valor1;
+    var valor2;
+    valor1 = prompt('Introducir primer número:', '');
+    valor2 = prompt('Introducir segundo número', '');
+    var suma = parseInt(valor1)+parseInt(valor2);
+    var producto = parseInt(valor1)*parseInt(valor2);
+    document.write('La suma es ');
+    document.write(suma);
+    document.write('<br>');
+    document.write('El producto es ');
+    document.write(producto);
+}
+
+function Aprobacion(){
+    
+}
+
