5ed742b7ee083fcedb4599a20b41ac4b8e212f49
Alanreb <alanrebgradas@gmail.com>
2024-09-26 14:48:04 -0600

    Ejercicio realizado hasta el 10

--- actividades/js/src/main.js
+++ actividades/js/src/main.js
@@ -54,6 +54,142 @@ function SumaProducto(){
 }
 
 function Aprobacion(){
-    
+    var nombre;
+    var nota;
+    nombre = prompt('Ingresa tu nombre:', '');
+    nota = prompt('Ingresa tu nota:', '');
+    if (nota>=4) {
+    document.write(nombre+' esta aprobado con un '+nota);
+    }
 }
 
+function MayorQue(){
+    var num1,num2;
+    num1 = prompt('Ingresa el primer número:', '');
+    num2 = prompt('Ingresa el segundo número:', '');
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    if (num1>num2) {
+    document.write('el mayor es '+num1);
+    }
+    else {
+    document.write('el mayor es '+num2);
+    }
+}
+
+function PromedioAprobado(){
+    var nota1,nota2,nota3;
+
+    nota1 = prompt('Ingresa 1ra. nota:', '');
+    nota2 = prompt('Ingresa 2da. nota:', '');
+    nota3 = prompt('Ingresa 3ra. nota:', '');
+    
+    //Convertimos los 3 string en enteros
+    nota1 = parseInt(nota1);
+    nota2 = parseInt(nota2);
+    nota3 = parseInt(nota3);
+    
+    var pro;
+    pro = (nota1+nota2+nota3)/3;
+    if (pro>=7) {
+            document.write('aprobado');
+        }
+        else {
+        if (pro>=4) {
+            document.write('regular');
+            }
+            else {
+            document.write('reprobado');
+            }
+        }
+
+}
+
+function ValorEntre1y5(){
+    var valor;
+    valor = prompt('Ingresar un valor comprendido entre 1 y 5:', '');
+    //Convertimos a entero
+    valor = parseInt(valor);
+    switch (valor) {
+    case 1: document.write('uno');
+    
+    break;
+    
+    case 2: document.write('dos');
+    
+    break;
+    
+    case 3: document.write('tres');
+    
+    break;
+    
+    case 4: document.write('cuatro');
+    
+    break;
+    
+    case 5: document.write('cinco');
+    
+    break;
+    
+    default:document.write('debe ingresar un valor comprendido entre 1 y 5.');
+    }
+}
+
+function ColorFondo(){
+    var col;
+    col = prompt('Ingresa el color con que quierar pintar el fondo de la ventana (rojo, verde, azul)' , '');
+    switch (col) {
+        case 'rojo': document.bgColor='#ff0000';
+        
+        break;
+        
+        case 'verde': document.bgColor='#00ff00';
+        
+        break;
+        
+        case 'azul': document.bgColor='#0000ff';
+        
+        break;
+    }
+}
+
+function UnoACien(){
+    var x;
+    x=1;
+    while (x<=100) {
+    document.write(x);
+    document.write('<br>');
+    x=x+1;
+    }
+}
+
+function ej11(){
+
+}
+
+function ej12(){
+
+}
+function ej13(){
+
+}
+
+function ej14(){
+
+}
+
+function ej15(){
+
+}
+
+function ej16(){
+
+}
+
+function ej17(){
+
+}
+
+function ej18(){
+
+}
