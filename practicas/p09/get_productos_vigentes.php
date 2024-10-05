fa18a559b4f5d0c1a6748a71df376f5fe7b1c8ea
Alanreb <alanrebgradas@gmail.com>
2024-10-04 19:13:01 -0600

    Ejemplos probados e insertados en p10 y correcciones a fromularios previos

--- practicas/p09/get_productos_vigentes.php
+++ practicas/p09/get_productos_vigentes.php
@@ -1,42 +1,34 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
 "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 	<?php
-	if(isset($_GET['tope']))
-		$tope = $_GET['tope'];
-
-	if (!empty($tope))
-	{
 		/** SE CREA EL OBJETO DE CONEXION */
 		@$link = new mysqli('localhost', 'root', 'nomeacuerdo', 'marketzone');	
 
 		/** comprobar la conexión */
-		if ($link->connect_errno) 
-		{
-			die('Falló la conexión: '.$link->connect_error.'<br/>');
-			    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+		if ($link->connect_errno) {
+			die('Falló la conexión: ' . $link->connect_error . '<br/>');
 		}
 
 		/** Crear una tabla que no devuelve un conjunto de resultados */
 		// MODIFICACIÓN: Se añade 'eliminado = 0' a la consulta para excluir productos eliminados
-		if ( $result = $link->query("SELECT * FROM productos WHERE unidades <= $tope AND eliminado = 0") )
-		{
-            /** Se extraen las tuplas obtenidas de la consulta */
+		if ($result = $link->query("SELECT * FROM productos WHERE eliminado = 0")) {
+			/** Se extraen las tuplas obtenidas de la consulta */
 			$row = $result->fetch_all(MYSQLI_ASSOC);
-
-            /** Se crea un arreglo con la estructura deseada */
-            foreach($row as $num => $registro) {            // Se recorren tuplas
-                foreach($registro as $key => $value) {      // Se recorren campos
-                    $data[$num][$key] = utf8_encode($value);
-                }
-            }
+			
+			/** Se crea un arreglo con la estructura deseada */
+			$data = [];
+			foreach ($row as $num => $registro) { // Se recorren tuplas
+				foreach ($registro as $key => $value) { // Se recorren campos
+					$data[$num][$key] = utf8_encode($value);
+				}
+			}
 
 			/** útil para liberar memoria asociada a un resultado con demasiada información */
 			$result->free();
 		}
 
 		$link->close();
-	}
 	?>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
@@ -48,7 +40,7 @@
 
 		<br/>
 		
-		<?php if( isset($data) && !empty($data) ) : ?>
+		<?php if (isset($data) && !empty($data)) : ?>
 
 			<table class="table">
 				<thead class="thead-dark">
@@ -64,7 +56,7 @@
 					</tr>
 				</thead>
 				<tbody>
-					<?php foreach($data as $row): ?>
+					<?php foreach ($data as $row): ?>
 						<tr>
 							<th scope="row"><?= htmlspecialchars($row['id']) ?></th>
 							<td><?= htmlspecialchars($row['nombre']) ?></td>
@@ -79,12 +71,10 @@
 				</tbody>
 			</table>
 
-			<?php elseif(!empty($tope)) : ?>
-
+		<?php else: ?>
 			<script>
-				alert('No se encontraron productos con el tope de unidades especificado');
+				alert('No se encontraron productos disponibles.');
 			</script>
-
 		<?php endif; ?>
 
 	</body>
--- practicas/p09/set_producto_v2.php
+++ practicas/p09/set_producto_v2.php
@@ -1,42 +1,52 @@
 <?php
+    // Obtener los valores del formulario
     $nombre = $_POST['name'];
     $marca  = $_POST['marca'];
     $modelo = $_POST['modelo'];
     $precio = $_POST['precio'];
     $detalles = $_POST['detalles'];
     $unidades = $_POST['unidades'];
-    $imagen   = $_POST['imagen'];
-    $eliminado =0;
+    $eliminado = 0;
 
-    /** SE CREA EL OBJETO DE CONEXION */
-    @$link = new mysqli('localhost', 'root', 'nomeacuerdo', 'marketzone');	
-
-    /** comprobar la conexión */
-    if ($link->connect_errno) 
-    {
-        die('Falló la conexión: '.$link->connect_error.'<br/>');
-        /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+    // Conectar a la base de datos
+    @$link = new mysqli('localhost', 'root', 'nomeacuerdo', 'marketzone');
+    if ($link->connect_errno) {
+        die('Falló la conexión: '.$link->connect_error);
     }
 
-    /** Crear una tabla que no devuelve un conjunto de resultados */
-    $sql = "INSERT INTO productos (nombre, marca, modelo, precio, detalles, unidades,imagen) VALUES ('{$nombre}', '{$marca}', '{$modelo}', '{$precio}', '{$detalles}', '{$unidades}', '{$imagen}');";
-    
-    
-    
-    if ( $link->query($sql) ) 
-    {
-        //if($nombre && $marca && $modelo)
-        //{
-            echo 'Producto insertado con ID: '.$link->insert_id;
-        //}
-        //else    
-        //{
-        //    echo 'El Producto no pudo ser insertado =(';
-        //}
+    // Ruta por defecto si no se sube una imagen
+    $defaultImagePath = 'tecweb\practicas\p08\img\defecto.jpg';
+
+    // Verificar si se ha subido una imagen
+    if (isset($_FILES['imagen']) && $_FILES['imagen']['error'] === UPLOAD_ERR_OK) {
+        // Información de la imagen subida
+        $nombreImagen = $_FILES['imagen']['name'];
+        $tmpImagen = $_FILES['imagen']['tmp_name'];
+        
+        // Mover la imagen a un directorio en el servidor
+        $uploadDir = '../p08/img/'; // Carpeta donde se almacenarán las imágenes
+        $rutaImagen = $uploadDir . basename($nombreImagen);
+
+        // Intentar mover el archivo subido al directorio de destino
+        if (move_uploaded_file($tmpImagen, $rutaImagen)) {
+            echo "La imagen ha sido subida correctamente.";
+        } else {
+            echo "Error al subir la imagen.";
+            $rutaImagen = $defaultImagePath; // Usar la imagen por defecto en caso de error
+        }
+    } else {
+        // Si no se ha subido una imagen, usar la ruta por defecto
+        $rutaImagen = $defaultImagePath;
     }
-    else
-    {
-        echo 'El Producto no pudo ser insertado =(';
+
+    // Crear la consulta para insertar el producto
+    $sql = "INSERT INTO productos (nombre, marca, modelo, precio, detalles, unidades, imagen, eliminado)
+            VALUES ('{$nombre}', '{$marca}', '{$modelo}', '{$precio}', '{$detalles}', '{$unidades}', '{$rutaImagen}', '{$eliminado}')";
+
+    if ($link->query($sql)) {
+        echo 'Producto insertado con ID: ' . $link->insert_id;
+    } else {
+        echo 'Error al insertar el producto: ' . $link->error;
     }
 
     $link->close();
