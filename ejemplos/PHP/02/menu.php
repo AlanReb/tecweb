d80a3f7ec49ebdf7b27fe31dba8b95534195e690
Alanreb <alanrebgradas@gmail.com>
2024-10-24 14:36:28 -0600

    ejemplo php 2 finalizado

--- ejemplos/PHP/02/menu.php
+++ ejemplos/PHP/02/menu.php
@@ -9,7 +9,13 @@ class Menu {
     }
 
     public function mostrar() {
-        
+        for($i=0; $i<count($this->enlaces); $i++) {
+            echo '<a href="'.$this->enlaces[$i].'" >'.$this->titulos[$i].'</a>';
+
+            if($i < count($this->enlaces)-1 ){
+                echo ' - ';
+            }
+        }
     }
 }
 ?>
